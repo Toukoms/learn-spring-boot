dd542a616cd30e8b65ffbbff290325bce0b18a75
Tokiniaina <tokiniaina090605@gmail.com>
2025-04-21 11:40:01 +0300

    Refactor the TrainingApplication removing unused method CommandLineRunner

--- src/main/java/com/example/training/TrainingApplication.java
+++ src/main/java/com/example/training/TrainingApplication.java
@@ -1,31 +1,13 @@
 package com.example.training;
 
-import com.example.training.data.repository.EmployeeRepository;
-import com.example.training.data.repository.RoomRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TrainingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrainingApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner run(RoomRepository roomRepository, EmployeeRepository employeeRepository) {
-		return args -> {
-			System.out.print("**************** ROOMS ******************");
-			System.out.println(roomRepository.findAll().isEmpty() ? "No Room in database" : "");
-			roomRepository.findAll().forEach(System.out::println);
-
-			System.out.println();
-			System.out.print("**************** EMPLOYEES ******************");
-			System.out.println(employeeRepository.findAll().isEmpty() ? "No Employee in database" : "");
-			employeeRepository.findAll().forEach(System.out::println);
-		};
 	}
 
 }
